b106c081dec0231b53cff573326c1b8f742eada3
a.v.verkhoglyad <a.v.verkhoglyad@gmail.com>
2023-06-03 14:58:32 +0300

    stepic: ex.2.4 - instagram card p.1

---
+++ src/jvmMain/kotlin/stepic/InstagramCard.kt
@@ -0,0 +1,115 @@
+package stepic
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+@Preview
+fun InstagramCardPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
+        InstagramCard()
+    }
+}
+
+@Composable
+fun InstagramCard() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(width = 1.dp, Color.DarkGray)
+            .padding(5.dp)
+    ) {
+        Column {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(1.dp, Color.Cyan)
+                    .padding(5.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Box(
+                    modifier = Modifier
+                        .width(100.dp)
+                        .padding(5.dp)
+                ) {
+                    Img()
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1F)
+                        .padding(5.dp)
+                ) {
+                    Metric(value = 6950, text = "Posts", modifier = Modifier.fillMaxWidth())
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1F).padding(5.dp)
+                ) {
+                    Metric(value = 436, text = "Followers", modifier = Modifier.fillMaxWidth())
+                }
+                Box(
+                    modifier = Modifier.fillMaxWidth()
+                        .weight(1F)
+                        .padding(5.dp)
+                ) {
+                    Metric(value = 76, text = "Following", modifier = Modifier.fillMaxWidth())
+                }
+            }
+            Row {
+                Column {
+                    Text("Instagram")
+                    Text("#HashTag")
+                    Text("https://some.url")
+                    Button({}) {
+                        Text("Follow")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Img(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.width(70.dp)
+            .border(1.dp, Color.Cyan)
+            .padding(5.dp)
+    ) {
+        Box(
+            modifier = modifier
+                .background(Color.Magenta)
+                .size(50.dp)
+        )
+    }
+}
+
+@Composable
+private fun Metric(value: Int, text: String, modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .border(1.dp, Color.Cyan)
+            .padding(5.dp)
+    ) {
+        Text(value.toString())
+        Text(text)
+    }
+}
